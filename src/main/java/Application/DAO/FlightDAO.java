fb046e30e58047bc0a8ef6e257197f5e3b9e27be
dythirk <dythirk@github.com>
2025-12-08 12:08:20 +0000

    Test run: 6/13 passed, 7 failed, 0 skipped - 2025-12-08T12:08:20.081Z

--- src/main/java/Application/DAO/FlightDAO.java
+++ src/main/java/Application/DAO/FlightDAO.java
@@ -33,7 +33,7 @@ public class FlightDAO {
         List<Flight> flights = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "SELECT * from Flight";
+            String sql = "SELECT * from flight";
 
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
@@ -152,14 +152,14 @@ public class FlightDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "UPDATE Flight set departure_city=?, arrival_city=? where flight_id = ?;";
+            String sql = "UPDATE flight set departure_city=?, arrival_city=? where flight_id = ?;";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write PreparedStatement setString and setInt methods here.
 
             preparedStatement.setString(1,flight.getDeparture_city());
             preparedStatement.setString(2,flight.getArrival_city());
-            preparedStatement.setInt(3,id);
+            preparedStatement.setInt(3,flight.getFlight_id());
 
 
             preparedStatement.executeUpdate();
